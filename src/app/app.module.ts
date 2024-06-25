eb53b183bcb47150c5955d90c9e32965a825227d
craigraphics <craigraphics@mail.com>
2024-06-25 16:18:59 -0700

    - added actions and reducers - added book-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookReducer } from './books/book.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
+    StoreModule.forRoot({ book: BookReducer }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
